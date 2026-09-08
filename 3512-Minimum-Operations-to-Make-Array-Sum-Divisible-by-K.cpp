4235b92812320158951cd1de425609ac20386b9a
HARSHAA <harshstuffs@gmail.com>
2026-09-08 15:45:25 +0530

    [3512] [Time Beats: 100.00%] [Memory Beats: 68.81%] - LeetPush

---
+++ 3512-Minimum-Operations-to-Make-Array-Sum-Divisible-by-K.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int tot=accumulate(nums.begin(),nums.end(),0);
+        int rem=tot%k;
+        return rem;
+    }
+};
